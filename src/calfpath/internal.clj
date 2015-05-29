ed13d767e54cb38a1e51bdc9dab8d11bff94c971
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-05-29 21:34:39 +0530

    optimize pattern making and URI matching

--- src/calfpath/internal.clj
+++ src/calfpath/internal.clj
@@ -19,39 +19,44 @@
         (let [ch (.charAt route i)
               [jn s? r] (if s?
                           (if (= ^char marker-char ch)
-                            [(inc i) false (conj r (.substring route j i))]
+                            [(unchecked-inc i) false (conj r (.substring route j i))]
                             [j true r])
                           (if (= separator ch)
                             [i true  (conj r (keyword (.substring route j i)))]
                             [j false r]))]
-          (recur (inc i) (int jn) s? r))))))
+          (recur (unchecked-inc i) (int jn) s? r))))))
 
 
 (defn match-uri
   "Given a URI string and an array of pattern tokens (string or keyword only) return a map of keywords to string param
   values on a successful match, nil otherwise."
-  [^String uri pattern-tokens]
-  (let [n (.length uri)
-        separator (int \/)]
-    (loop [i (int 0) ; index into the URI
-           j (int 0) ; index into pattern-tokens
-           p {}      ; URI params
-           ]
-      (if (>= i n)
-        p
-        (let [t (get pattern-tokens j)
-              [r? in jn pn] (if (string? t)
-                              ;; match URI string from current index position
-                              (when (.startsWith uri ^String t i)
-                                [true (+ i (.length ^String t)) (inc j) p])
-                              ;; assume keyword, extract the URI param
-                              (let [stop (.indexOf uri separator i)
-                                    stop (if (>= stop 0)
-                                           stop
-                                           n)]
-                                [true stop (inc j) (assoc p t (.substring uri i stop))]))]
-          (when r?
-            (recur (int in) (int jn) pn)))))))
+  [^String uri ^java.util.List pattern-tokens]
+  (if (= 1 (.size pattern-tokens))
+    ;; if it's just one token, it must be a string
+    (when (.equals uri ^String (first pattern-tokens))
+      {})
+    (let [n (.length uri)
+          p (transient {}) ; URI params
+          separator (int \/)]
+      (loop [i (int 0) ; index into the URI
+             j (int 0) ; index into pattern-tokens
+             ]
+        (if (>= i n)
+          (persistent! p)
+          (let [t (get pattern-tokens j)
+                [r? in jn] (if (instance? String t)
+                             ;; match URI string from current index position
+                             (when (.startsWith uri ^String t i)
+                               [true (+ i (.length ^String t)) (unchecked-inc j)])
+                             ;; assume keyword, extract the URI param
+                             (let [stop (.indexOf uri separator i)
+                                   stop (if (>= stop 0)
+                                          stop
+                                          n)]
+                               (assoc! p t (.substring uri i stop))
+                               [true stop (unchecked-inc j)]))]
+            (when r?
+              (recur (int in) (int jn)))))))))
 
 
 (def valid-method-keys #{:get :head :options :put :post :delete})
