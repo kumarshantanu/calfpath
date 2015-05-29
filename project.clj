07dffd11c8b58ea2ef190600d13eee351cc734f8
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-05-29 21:32:58 +0530

    enable reflection warning and assertions

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/kumarshantanu/calfpath"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :global-vars {*warn-on-reflection* true
+                *assert* true
+                *unchecked-math* :warn-on-boxed}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-RC1"]
                                   [org.clojure/tools.nrepl "0.2.10"]]}
              :perf {:dependencies [[compojure    "1.3.4"]
