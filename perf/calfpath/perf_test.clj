c4bf55c6c2b109842377ae7adac63acfec97fe3f
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-06-02 11:38:25 +0530

    add clout benchmarks, (is (= ..)) correctness

--- perf/calfpath/perf_test.clj
+++ perf/calfpath/perf_test.clj
@@ -1,9 +1,10 @@
 (ns calfpath.perf-test
   (:require
-    [clojure.test :refer [deftest testing use-fixtures]]
+    [clojure.test :refer [deftest is testing use-fixtures]]
     [compojure.core :refer [defroutes rfn routes context GET POST PUT ANY]]
+    [clout.core     :as l]
     [calfpath.core  :refer [->uri ->method ->get ->head ->options ->put ->post ->delete]]
-    [citius.core   :as c]))
+    [citius.core    :as c]))
 
 
 (defroutes handler-compojure
@@ -15,9 +16,9 @@
                       :headers {"Content-Type" "text/plain"}
                       :body "1.2"})
     (ANY "/" request {:status 405
-                      :headers {"Allow" "GET"
+                      :headers {"Allow" "GET, PUT"
                                 "Content-Type" "text/plain"}
-                      :body "405 Method not supported. Supported methods are: GET"}))
+                      :body "405 Method not supported. Supported methods are: GET, PUT"}))
   (context "/user/:id/permissions"    [id]
     (GET "/" request {:status 200
                       :headers {"Content-Type" "text/plain"}
@@ -26,9 +27,9 @@
                       :headers {"Content-Type" "text/plain"}
                       :body "2.2"})
     (ANY "/" request {:status 405
-                      :headers {"Allow" "GET"
+                      :headers {"Allow" "GET, PUT"
                                 "Content-Type" "text/plain"}
-                      :body "405 Method not supported. Supported methods are: GET"}))
+                      :body "405 Method not supported. Supported methods are: GET, PUT"}))
   (context "/company/:cid/dept/:did"  [cid did]
     (PUT "/" request {:status 200
                       :headers {"Content-Type" "text/plain"}
@@ -36,7 +37,7 @@
     (ANY "/" request {:status 405
                       :headers {"Allow" "PUT"
                                 "Content-Type" "text/plain"}
-                      :body "405 Method not supported. Supported methods are: PUT"}))
+                      :body "405 Method not supported. Only PUT is supported."}))
   (context "/this/is/a/static/route"  []
     (PUT "/" request {:status 200
                       :headers {"Content-Type" "text/plain"}
@@ -44,10 +45,64 @@
     (ANY "/" request {:status 405
                       :headers {"Allow" "PUT"
                                 "Content-Type" "text/plain"}
-                      :body "405 Method not supported. Supported methods are: PUT"}))
+                      :body "405 Method not supported. Only PUT is supported."}))
   (rfn request {:status 400
                 :headers {"Content-Type" "text/plain"}
-                :body "400 Bad request. URI does not match any available route."}))
+                :body "400 Bad request. URI does not match any available uri-template."}))
+
+
+(let [uri-1 (l/route-compile "/user/:id/profile/:type/")
+      uri-2 (l/route-compile "/user/:id/permissions/")
+      uri-3 (l/route-compile "/company/:cid/dept/:did/")
+      uri-4 (l/route-compile "/this/is/a/static/route")]
+  (defn handler-clout
+    [request]
+    (condp l/route-matches request
+      uri-1 (let [{:keys [id type]} request]
+                                        (case (:request-method request)
+                                          :get {:status 200
+                                                :headers {"Content-Type" "text/plain"}
+                                                :body "1.1"}
+                                          :put {:status 200
+                                                :headers {"Content-Type" "text/plain"}
+                                                :body "1.2"}
+                                          {:status 405
+                                           :headers {"Allow" "GET, PUT"
+                                                     "Content-Type" "text/plain"}
+                                           :body "405 Method not supported. Supported methods are: GET, PUT"}))
+      uri-2 (let [{:keys [id]} request]
+                                      (case (:request-method request)
+                                        :get {:status 200
+                                              :headers {"Content-Type" "text/plain"}
+                                              :body "2.1"}
+                                        :put {:status 200
+                                              :headers {"Content-Type" "text/plain"}
+                                              :body "2.2"}
+                                        {:status 405
+                                         :headers {"Allow" "GET, PUT"
+                                                   "Content-Type" "text/plain"}
+                                         :body "405 Method not supported. Supported methods are: GET, PUT"}))
+      uri-3 (let [{:keys [cid did]} request]
+                                        (if (identical? :put (:request-method request))
+                                          {:status 200
+                                           :headers {"Content-Type" "text/plain"}
+                                           :body "3"}
+                                          {:status 405
+                                           :headers {"Allow" "PUT"
+                                                     "Content-Type" "text/plain"}
+                                           :body "405 Method not supported. Only PUT is supported."}))
+      uri-4 (let []
+                                       (if (identical? :put (:request-method request))
+                                         {:status 200
+                                          :headers {"Content-Type" "text/plain"}
+                                          :body "4"}
+                                         {:status 405
+                                          :headers {"Allow" "PUT"
+                                                    "Content-Type" "text/plain"}
+                                          :body "405 Method not supported. Only PUT is supported."}))
+      {:status 400
+       :headers {"Content-Type" "text/plain"}
+       :body "400 Bad request. URI does not match any available uri-template."})))
 
 
 (defn handler-calfpath
@@ -75,31 +130,38 @@
                                                          :body "4"})))
 
 
-(use-fixtures :once (c/make-bench-wrapper ["Compojure" "CalfPath"]
-                      {:chart-title "Compojure vs CalfPath"
+(use-fixtures :once (c/make-bench-wrapper ["Compojure" "Clout" "CalfPath"]
+                      {:chart-title "Compojure/Clout/CalfPath"
                        :chart-filename (format "bench-clj-%s.png" c/clojure-version-str)}))
+
+
+(defmacro test-compare-perf
+  [bench-name & exprs]
+  `(do
+     (is (= ~@exprs) ~bench-name)
+     (c/compare-perf ~bench-name ~@exprs)))
 
 
 (deftest test-no-match
   (testing "no URI match"
     (let [request {:request-method :get
                    :uri "/hello/joe/"}]
-      (c/compare-perf "no URI match" (handler-compojure request) (handler-calfpath request))))
+      (test-compare-perf "no URI match" (handler-compojure request) (handler-clout request) (handler-calfpath request))))
   (testing "no method match"
     (let [request {:request-method :put
                    :uri "/user/1234/profile/compact/"}]
-      (c/compare-perf "no method match" (handler-compojure request) (handler-calfpath request)))))
+      (test-compare-perf "no method match" (handler-compojure request) (handler-clout request) (handler-calfpath request)))))
 
 
 (deftest test-match
   (testing "static route match"
     (let [request {:request-method :put
                    :uri "/this/is/a/static/route"}]
-      (c/compare-perf "static URI match, 1 method" (handler-compojure request) (handler-calfpath request))))
+      (test-compare-perf "static URI match, 1 method" (handler-compojure request) (handler-clout request) (handler-calfpath request))))
   (testing "pattern route match"
     (let [request {:request-method :get
                    :uri "/user/1234/profile/compact/"}]
-      (c/compare-perf "pattern URI match, 2 methods" (handler-compojure request) (handler-calfpath request)))
+      (test-compare-perf "pattern URI match, 2 methods" (handler-compojure request) (handler-clout request) (handler-calfpath request)))
     (let [request {:request-method :get
                    :uri "/company/1234/dept/5678/"}]
-      (c/compare-perf "pattern URI match, 1 method" (handler-compojure request) (handler-calfpath request)))))
+      (test-compare-perf "pattern URI match, 1 method" (handler-compojure request) (handler-clout request) (handler-calfpath request)))))
