fdea34d8a1f961da685fba066ef05ec0c275a681
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-03-08 17:24:16 +0530

    kick off 0.3.0 development

--- CHANGES.md
+++ CHANGES.md
@@ -1,10 +1,17 @@
 # Changes and TODO
 
-## 2015-June-06 / 0.2.0
+## 0.3.0 / 2016-March-??
+
+* [TODO] Support for route-map (route-spec collection) as a core abstraction
+* [TODO] Refactor `make-uri-handler` using route-map
+
+
+## 0.2.0 / 2015-June-06
 
 * Dispatch (fn) on URI template by calling fns, that returns a Ring handler fn
 
-## 2015-June-04 / 0.1.0
+
+## 0.1.0 / 2015-June-04
 
 * Dispatch (macro) on URI template by evaluating expression in lexical scope
 * Dispatch (macro) on HTTP method by evaluating expression
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject calfpath "0.2.0"
+(defproject calfpath "0.3.0-SNAPSHOT"
   :description "A la carte ring request matching"
   :url "https://github.com/kumarshantanu/calfpath"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,9 @@
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
-             :perf {:dependencies [[compojure "1.4.0" :exclusions [[org.clojure/clojure]]]
+             :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
+                   :global-vars {*unchecked-math* :warn-on-boxed}}
+             :perf {:dependencies [[compojure "1.5.0" :exclusions [[org.clojure/clojure]]]
                                    [citius    "0.2.1"]]
                     :test-paths ["perf"]
                     :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]}})
