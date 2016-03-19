122e54335fa7647db42301c7c1909c2c76b735dd
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-03-20 01:11:44 +0530

    add additional test profiles

--- project.clj
+++ project.clj
@@ -8,11 +8,13 @@
   :java-source-paths ["java-src"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}
+             :c15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
+             :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :perf {:dependencies [[compojure "1.5.0" :exclusions [[org.clojure/clojure]]]
                                    [citius    "0.2.1"]]
                     :test-paths ["perf"]
