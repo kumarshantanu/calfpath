c7cd880a47b874bb81dc46ec76ddb8acdb75843d
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-10-12 01:08:09 +0530

    update dependency versions for compojure and clojure 1.9

--- project.clj
+++ project.clj
@@ -8,17 +8,16 @@
   :java-source-paths ["java-src"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}
              :c15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
-             :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha7"]]
+             :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :perf {:dependencies [[compojure "1.5.0" :exclusions [[org.clojure/clojure]]]
+             :perf {:dependencies [[compojure "1.5.1" :exclusions [[org.clojure/clojure]]]
                                    [citius    "0.2.3"]]
                     :test-paths ["perf"]
                     :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]}})
