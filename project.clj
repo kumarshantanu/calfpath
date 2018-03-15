7e12dcb1c2f50a00e9cfb3b81d3970d41121e947
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-03-15 16:24:27 +0530

    bump Compojure version to latest for perf benchmarks

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
              :c19 {:dependencies [[org.clojure/clojure "1.9.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :perf {:dependencies [[compojure "1.5.2" :exclusions [[org.clojure/clojure]]]
+             :perf {:dependencies [[compojure "1.6.0" :exclusions [[org.clojure/clojure]]]
                                    [citius    "0.2.4"]]
                     :test-paths ["perf"]
                     :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]}}
