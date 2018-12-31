7bc636d7c6f0bcb56e052433475ae421c414a642
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-12-31 15:03:14 +0530

    bump Bidi and Reitit versions to latest

--- project.clj
+++ project.clj
@@ -17,11 +17,11 @@
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :perf {:dependencies [[ataraxy   "0.4.2" :exclusions [[org.clojure/clojure]
                                                                    [ring/ring-core]]]
-                                   [bidi      "2.1.4" :exclusions [ring/ring-core]]
+                                   [bidi      "2.1.5" :exclusions [ring/ring-core]]
                                    [compojure "1.6.1" :exclusions [[org.clojure/clojure]
                                                                    [ring/ring-core]
                                                                    [ring/ring-codec]]]
-                                   [metosin/reitit-ring "0.2.9"]
+                                   [metosin/reitit-ring "0.2.10"]
                                    [citius    "0.2.4"]]
                     :test-paths ["perf"]
                     :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]}}
