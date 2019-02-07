79322d25f55eeef4891a54e8e89e22823f8caf25
Shantanu Kumar <kumar.shantanu@gmail.com>
2019-02-08 01:14:26 +0530

    bump Reitit version for perf tests

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
                                    [compojure "1.6.1" :exclusions [[org.clojure/clojure]
                                                                    [ring/ring-core]
                                                                    [ring/ring-codec]]]
-                                   [metosin/reitit-ring "0.2.10"]
+                                   [metosin/reitit-ring "0.2.13"]
                                    [citius    "0.2.4"]]
                     :test-paths ["perf"]
                     :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]}}
